67b314527a43e9b8da12c1a5bbc4730086b3b536
Brunopygit <163663180+Brunopygit@users.noreply.github.com>
2025-01-23 10:26:01 -0400

    testeee

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,21 @@
-<p>doideira</p>
+<style>  
+.background  {
+    background-color: rgb(15, 15, 15); /* Cor de fundo azul, em formato RGB */
+    color: rgb(255, 255, 255); /* Cor do texto */
+    font-family: Helvetica; /* Fonte para o texto */
+    font-size: 15px; /* Tamanho do texto moderado */
+    margin: 0;
+    padding: 0;
+    height: 100vh; /* Ocupa toda a altura da tela */
+    display: flex;
+    flex-direction: column;
+}
+</style>
+
+<div class = "background">
+
+<p>olá a todos</p>
+
+
+
+</div>
